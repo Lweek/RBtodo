95d46ee1084c912587b3c7e662e715dce3cdcd45
Vladimir Belohradsky <lionsart@seznam.cz>
2021-02-15 13:46:01 +0100

    chore: Create app boiler plate
    - renamed ContentView to MainView
    - renamed main app entry point
    - create accent color

---
+++ Todo Test/Todo Test/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo Test
+//
+//  Created by Vladimir Belohradsky on 15.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
---
+++ Todo Test/Todo Test/Views/MainView.swift
@@ -0,0 +1,21 @@
+//
+//  MainView.swift
+//  Todo Test
+//
+//  Created by Vladimir Belohradsky on 15.02.2021.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainView()
+    }
+}
