eb52f47bad6b863e8aa8aaf4327b8770b1a07ba1
Vladimir Belohradsky <lionsart@seznam.cz>
2021-02-15 16:34:15 +0100

    feat: Create TodoList model
    - implement Codable

---
+++ Todo Test/Todo Test/Models/Entities/TodoList.swift
@@ -0,0 +1,15 @@
+//
+//  TodoList.swift
+//  Todo Test
+//
+//  Created by Vladimir Belohradsky on 15.02.2021.
+//
+
+import Foundation
+
+struct TodoList: Codable {
+        
+    /// List of todos.
+    var todos: [Todo]
+    
+}
