d2e0bf0d5e8be7ed93dde97f668361d3691088bb
Vladimir Belohradsky <lionsart@seznam.cz>
2021-02-16 21:18:07 +0100

    feat: Connect all views into main view
    This view existed from boiler plate but didn't implement actual functionality.

--- Todo Test/Todo Test/Views/MainView.swift
+++ Todo Test/Todo Test/Views/MainView.swift
@@ -8,14 +8,58 @@
 import SwiftUI
 
 struct MainView: View {
+    
+    @EnvironmentObject var todosModelView: TodosModelView
+    
+    /// Is Add Todo view presented?
+    @State var isAddTodoPresented = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            switch todosModelView.state {
+            case .loading: LoadingView()
+            case .failure: FailureView() {
+                todosModelView.getTodos()
+            }
+            case .ready:
+            TodoListView()
+                .navigationTitle("main-title")
+                .toolbar {
+                    ToolbarItem(placement: .bottomBar) {
+                        Button(action: {
+                            isAddTodoPresented.toggle()
+                        }, label: {
+                            HStack {
+                                Image(systemName: "plus.circle.fill")
+                                Text("add-todo")
+                                    .font(.callout)
+                                    .fontWeight(.semibold)
+                            }
+                        })
+                    }
+                    ToolbarItem(placement: .bottomBar) {
+                        Spacer()
+                    }
+                }
+                .sheet(isPresented: self.$isAddTodoPresented) {
+                    AddTodoView() {
+                        isAddTodoPresented.toggle()
+                    }
+                }
+            }
+        }
+        .onAppear {
+            todosModelView.getTodos()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
+    
+    
+    
     static var previews: some View {
         MainView()
+            .environmentObject(TodosModelView(api: API(session: URLSession.shared)))
     }
 }
